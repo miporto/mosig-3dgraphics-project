69299b79288ebdefa184f75ee57effe4921078d6
Manuel Porto <manu.porto94@gmail.com>
2019-03-17 21:13:44 +0100

    Add resources and initial project files

---
+++ src/loaders.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Resources loader functions
+"""
+# Python built-in modules
+import os                           # os function, i.e. checking file status
+
+# External, non built-in modules
+import pyassimp                     # 3D resource loader
+import pyassimp.errors              # Assimp error management + exceptions
+
+# -------------- 3D ressource loader -----------------------------------------
+def load(file):
+    """ load resources from file using pyassimp, return list of ColorMesh """
+    try:
+        option = pyassimp.postprocess.aiProcessPreset_TargetRealtime_MaxQuality
+        scene = pyassimp.load(file, option)
+    except pyassimp.errors.AssimpError:
+        print('ERROR: pyassimp unable to load', file)
+        return []  # error reading => return empty list
+
+    meshes = [ColorMesh([m.vertices, m.normals], m.faces) for m in scene.meshes]
+    size = sum((mesh.faces.shape[0] for mesh in scene.meshes))
+    print('Loaded %s\t(%d meshes, %d faces)' % (file, len(scene.meshes), size))
+
+    pyassimp.release(scene)
+    return meshes
+
+# -------------- 3D textured mesh loader ---------------------------------------
+def load_textured(file):
+    """ load resources using pyassimp, return list of TexturedMeshes """
+    try:
+        option = pyassimp.postprocess.aiProcessPreset_TargetRealtime_MaxQuality
+        scene = pyassimp.load(file, option)
+    except pyassimp.errors.AssimpError:
+        print('ERROR: pyassimp unable to load', file)
+        return []  # error reading => return empty list
+
+    # Note: embedded textures not supported at the moment
+    path = os.path.dirname(file)
+    path = os.path.join('.', '') if path == '' else path
+    for mat in scene.materials:
+        mat.tokens = dict(reversed(list(mat.properties.items())))
+        if 'file' in mat.tokens:  # texture file token
+            tname = mat.tokens['file'].split('/')[-1].split('\\')[-1]
+            # search texture in file's whole subdir since path often screwed up
+            tname = [os.path.join(d[0], f) for d in os.walk(path) for f in d[2]
+                     if tname.startswith(f) or f.startswith(tname)]
+            if tname:
+                mat.texture = tname[0]
+            else:
+                print('Failed to find texture:', tname)
+
+    # prepare textured mesh
+    meshes = []
+    for mesh in scene.meshes:
+        texture = scene.materials[mesh.materialindex].texture
+
+        # tex coords in raster order: compute 1 - y to follow OpenGL convention
+        tex_uv = ((0, 1) + mesh.texturecoords[0][:, :2] * (1, -1)
+                  if mesh.texturecoords.size else None)
+
+        # create the textured mesh object from texture, attributes, and indices
+        meshes.append(TexturedMesh(texture, [mesh.vertices, tex_uv], mesh.faces))
+
+    size = sum((mesh.faces.shape[0] for mesh in scene.meshes))
+    print('Loaded %s\t(%d meshes, %d faces)' % (file, len(scene.meshes), size))
+
+    pyassimp.release(scene)
+    return meshes
