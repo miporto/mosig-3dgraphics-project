285f3c7c61ea32d2b744cff908a9237ac1a52c53
Manuel Porto <manu.porto94@gmail.com>
2019-03-18 11:49:46 +0100

    Add keyframes animations

--- src/viewer.py
+++ src/viewer.py
@@ -15,7 +15,8 @@ import pyassimp                     # 3D resource loader
 import pyassimp.errors              # Assimp error management + exceptions
 from PIL import Image               # load images for textures
 
-from transform import Trackball, identity, translate, rotate, scale
+from bisect import bisect_left      # search sorted keyframe lists
+from transform import Trackball, identity, translate, rotate, scale, lerp, vec, quaternion_slerp, quaternion_matrix, quaternion, quaternion_from_euler
 
 
 # ------------ low level OpenGL object wrappers ----------------------------
@@ -357,7 +358,57 @@ class TexturedMesh:
         # leave clean state for easier debugging
         GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
         GL.glUseProgram(0)
-        
+      
+# -------------- Interpolator class -----------------------------------------
+class KeyFrames:
+    """ Stores keyframe pairs for any value type with interpolation_function"""
+    def __init__(self, time_value_pairs, interpolation_function=lerp):
+        if isinstance(time_value_pairs, dict):  # convert to list of pairs
+            time_value_pairs = time_value_pairs.items()
+        keyframes = sorted(((key[0], key[1]) for key in time_value_pairs))
+        self.times, self.values = zip(*keyframes)  # pairs list -> 2 lists
+        self.interpolate = interpolation_function
+
+    def value(self, time):
+        """ Computes interpolated value from keyframes, for a given time """
+
+        # 1. ensure time is within bounds else return boundary keyframe
+        if time <= self.times[0]:
+            return self.values[0] # i need to return the values not the time
+        if time >= self.times[-1]:
+            return self.values[-1]
+        # 2. search for closest index entry in self.times, using bisect_left function
+        index_entry = bisect_left(self.times, time)
+        # 3. using the retrieved index, interpolate between the two neighboring values
+        # in self.values, using the initially stored self.interpolate function
+        fraction = (time - self.times[index_entry - 1]) / (self.times[index_entry] - self.times[index_entry-1])
+        return self.interpolate(self.values[index_entry - 1], self.values[index_entry], fraction)
+
+class TransformKeyFrames:
+    """ KeyFrames-like object dedicated to 3D transforms """
+    def __init__(self, translate_keys, rotate_keys, scale_keys):
+        """ stores 3 keyframe sets for translation, rotation, scale """
+        self.translate_keys = KeyFrames(translate_keys)
+        self.rotate_keys = KeyFrames(rotate_keys, quaternion_slerp)
+        self.scale_keys = KeyFrames(scale_keys)
+
+    def value(self, time):
+        """ Compute each component's interpolation and compose TRS matrix """
+        translate_mat = translate(self.translate_keys.value(time))
+        rotate_mat = quaternion_matrix(self.rotate_keys.value(time))
+        scale_mat = scale(self.scale_keys.value(time))
+        return translate_mat @ rotate_mat @ scale_mat
+
+class KeyFrameControlNode(Node):
+    """ Place node with transform keys above a controlled subtree """
+    def __init__(self, translate_keys, rotate_keys, scale_keys, **kwargs):
+        super().__init__(**kwargs)
+        self.keyframes = TransformKeyFrames(translate_keys, rotate_keys, scale_keys)
+
+    def draw(self, projection, view, model, **param):
+        """ When redraw requested, interpolate our node transform from keys """
+        self.transform = self.keyframes.value(glfw.get_time())
+        super().draw(projection, view, model, **param)
         
 # -------------- 3D ressource loader -----------------------------------------
 def load(file):
@@ -524,6 +575,13 @@ def main():
     """ create a window, add scene objects, then run rendering loop """
     viewer = Viewer()
 
+    translate_keys = {0: vec(0, 0, 0), 2: vec(1, 1, 0), 4: vec(0, 0, 0)}
+    rotate_keys = {0: quaternion(), 2: quaternion_from_euler(180, 45, 90),
+                   4: quaternion_from_euler(180, 0, 180), 6: quaternion()}
+    scale_keys = {0: 1, 2: 0.5, 4: 1}
+    keynode = KeyFrameControlNode(translate_keys, rotate_keys, scale_keys)
+    keynode.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file)])
+    viewer.add(keynode)
 
     #txt = TexturedPlane('resources/grass.png')
     #viewer.add(txt)
@@ -532,7 +590,7 @@ def main():
         print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
               ' format supported by pyassimp.' % (sys.argv[0],))
     
-    viewer.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file)])
+    #viewer.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file)])
     # start rendering loop
     viewer.run()
 
