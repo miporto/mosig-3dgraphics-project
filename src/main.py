b2ae80f3f9d4949d355aefcc573d8ec8bf2f3e4c
Manuel Porto <manu.porto94@gmail.com>
2019-03-31 17:23:14 +0200

    Set origin coordinates for spaceship

--- src/main.py
+++ src/main.py
@@ -9,10 +9,17 @@ import sys
 import glfw                         # lean window system wrapper for OpenGL
 
 from keyframe import KeyFrameControlNode
-from loaders import load_textured
+from loaders import load, load_textured
 from skybox import Skybox
 from transform import vec, quaternion, quaternion_from_euler
 from viewer import Viewer
+from node import Node
+
+class Cylinder(Node):
+    """ Very simple cylinder based on practical 2 load function """
+    def __init__(self):
+        super().__init__()
+        self.add(*load('./res/cylinder.obj'))  # just load the cylinder from file
 
 def main():
     """ create a window, add scene objects, then run rendering loop """
@@ -23,14 +30,17 @@ def main():
         print('Usage:\n\t%s [3dfile]*\n\n3dfile\t\t the filename of a model in'
               ' format supported by pyassimp.' % (sys.argv[0],))
     
-    translate_keys = {0: vec(0, 0, 0), 2: vec(0, .2, 0), 4: vec(0.3, 0, 0)}
-    rotate_keys = {0: quaternion(), 2: quaternion_from_euler(180, 0, 180),
-                   4: quaternion_from_euler(180, 0, 180), 6: quaternion()}
-    scale_keys = {0: 0.5, 2: 0.3, 4: 0.6}
+    #translate_keys = {0: vec(0, 0, 0), 10: vec(1, 0, 0), 15: vec(0, 1, 0), 20: vec(0, 0, 1), 25: vec(0, 0, 0)}
+    translate_keys = {0: vec(0.75, 0, -1)} 
+    # rotate_keys = {0: quaternion(), 2: quaternion_from_euler(180, 0, 180),
+    #                4: quaternion_from_euler(180, 0, 180), 6: quaternion()}
+    rotate_keys = {0: quaternion()}
+    #scale_keys = {0: 0.125, 2: 0.25, 4: 0.5}
+    scale_keys = {0: 0.5}
     keynode = KeyFrameControlNode(translate_keys, rotate_keys, scale_keys)
     keynode.add(*[mesh for file in sys.argv[1:] for mesh in load_textured(file)])
     viewer.add(keynode)
-
+    #viewer.add(Cylinder())
     #txt = TexturedPlane('resources/grass.png')
     #viewer.add(txt)
 
