a4aaa45f6cb9fa0ff1d2de341b3722d74179999e
Manuel Porto <manu.porto94@gmail.com>
2019-04-14 18:17:36 +0200

    Translate UFO

--- src/ufo.py
+++ src/ufo.py
@@ -5,5 +5,5 @@ from node import Node
 class UFO(Node):
     """Textured bunny"""
     def __init__(self):
-        super().__init__(transform=(scale(0.009) @ translate(0.92, -0.32, -1)))
+        super().__init__(transform=(scale(0.009) @ translate(3.92, -8.32, 0)))
         self.add(*load_textured('res/ufo/Low_poly_UFO.obj'))
